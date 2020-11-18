0b52c30f2b18ecb444f4b982b18f41ef789cf586
Karla Navas <dev@karlanavas.com>
2020-11-19 02:50:18 +1100

    implementing logic for controller functions

--- app/Http/Controllers/SkillsController.php
+++ app/Http/Controllers/SkillsController.php
@@ -3,14 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Skill;
 
 class SkillsController extends Controller
 {
     public function list(){
-        return response()->json('list');
+        return response()->json(Skill::all());
     }
 
-    public function getById(){
-        return response()->json('getById');
+    public function getById($id){
+        $skill = Skill::find($id);
+
+        if(!$skill){
+            abort(404, 'Skill with id: '.$id.' was not found');
+        }
+
+        return response()->json($skill);
     }
 }
